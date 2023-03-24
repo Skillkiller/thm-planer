b1205f2bcdbcd5e8c13961e3aee88452dfc1cfb6
Skillkiller <seblsebastian@aol.de>
2023-03-24 20:46:24 +0100

    Add footer with git information

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,28 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const commitHashShort = require('child_process')
+  .execSync('git rev-parse --short HEAD')
+  .toString()
+  .trim()
+
+const commitHashLong = require('child_process')
+  .execSync('git rev-parse HEAD')
+  .toString()
+  .trim()
+
+const commitDatetime = require('child_process')
+  .execSync('git show -s --format=%ci ' + commitHashLong)
+  .toString()
+  .trim()
+
+const commitAuthor = require('child_process')
+  .execSync('git show -s --format=%an ' + commitHashLong)
+  .toString()
+  .trim()
+
+
+
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
@@ -31,7 +53,13 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      templateParameters: {
+        __COMMIT_HASH_SHORT__: commitHashShort,
+        __COMMIT_HASH_LONG__: commitHashLong,
+        __COMMIT_AUTHOR__: commitAuthor,
+        __COMMIT_DATE__: commitDatetime,
+      }
     })
   ]
 }
